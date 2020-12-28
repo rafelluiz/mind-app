19bdbb3338634611d69a60767a1ef8784480d038
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-12-28 09:35:13 -0300

    Adiciona deleção de conteúdo
    Tarefa: https://www.notion.so/Criar-CRUD-de-Conte-do-d736d407bdda4a308224b06dacabdd4d

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,12 +1,12 @@
 class ContentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_content, only: [:show,:edit,:update,:destroy]
 
   def index
     @contents = current_user.contents
   end
 
   def show
-    @content = Content.find(params[:id])
   end
 
   def new
@@ -24,12 +24,9 @@ class ContentsController < ApplicationController
   end
 
   def edit
-    @content = Content.find(params[:id])
   end
 
   def update
-    @content = Content.find(params[:id])
-
     if @content.update(content_params)
       redirect_to contents_path, notice: 'Content was successfully updated.'
     else
@@ -37,7 +34,18 @@ class ContentsController < ApplicationController
     end
   end
 
+  def destroy
+    @content = Content.find(params[:id])
+    @content.destroy
+
+    redirect_to contents_path, notice: 'Content was successfully destroyed.'
+  end
+
   private
+
+  def set_content # para nao repetir codigos
+    @content = Content.find(params[:id])
+  end
 
   def content_params
     params.require(:content).permit(:title,:description)
