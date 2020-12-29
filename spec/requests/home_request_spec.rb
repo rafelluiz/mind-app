90efd71d92a5d36dec5f92ad70f225efe4a154c4
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-12-28 23:15:32 -0300

    Adiciona teste para modelo Tag e ajusta teste home controller

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe "Homes", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+      get "/home"
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
